cbd4e8c58258167d967314ea753610543059dd51
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2024-10-06 15:34:33 +0500

    complete first part of project.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// AOS.init();
-
 $(document).ready(function(){
     $(".product-carousel").slick({
         dots: true,
@@ -32,25 +30,33 @@ $(document).ready(function(){
 $(document).ready(function() {
     $(".hero-carousel").slick({
         infinite: true,
-        speed: 1000,
+        speed: 800,
         fade: true,
         autoplay: true,
         cssEase: "linear",
     })
 });
 
-// $(".hero-carousel").on("beforeChange", function(event, slick) {
-//   console.log("hello");
-//   // AOS.refreshHard();
-// })
+AOS.init({
+  once: true,
+});
 
-$(".hero-carousel .btn--primary").on("click", function(event) {
-  console.log("clicked");
+$(".hero-carousel").on("beforeChange", function(event) {
+  document.querySelectorAll(".aos-animate").forEach((element) => {
+    element.classList.remove("aos-animate");
+  })
+})
 
-  // document.querySelectorAll('[data-aos]').forEach((element) => {
-  //     element.setAttribute('data-aos', element.getAttribute('data-aos'));
-  // });
-  
-  // AOS.refresh();
-  AOS.init();
+$(".hero-carousel").on("afterChange", function(event) {
+  document.querySelectorAll(".aos-init").forEach((element) => {
+    element.classList.add("aos-animate");
+  })
+})
+
+
+// Logic for changing flag at language select element.
+const langDropdown = document.querySelector(".info-bar__lang");
+langDropdown.addEventListener("change", (e) => {
+  const value = langDropdown.children[1].value;
+  langDropdown.children[0].setAttribute("src", `./assests/img/lang-flags/${value}.jpg`)
 })
